7d8b88c4c25f6fe02fec2defca56dff13b229ea1
edmar.james <edmarjamesbautista@gmail.com>
2023-08-13 17:43:26 +0800

    Use useReducer for handling states on CartProvider

--- project-7/src/components/Layout/HeaderCartButton.js
+++ project-7/src/components/Layout/HeaderCartButton.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
 
 import CartIcon from '../Cart/CartIcon';
 import styles from './HeaderCartButton.module.css';
+import CartContext from '../../store/cart-context';
 
 export default function HeaderCartButton({ onClick }) {
+
+    const { items } = useContext(CartContext);
+
+    const numberOfCartItems = items.reduce((currentNumber, item) => {
+        return currentNumber + item.amount;
+    }, 0);
+
     return (
         <button className={styles.button} onClick={onClick}>
             <span className={styles.icon}>
@@ -11,7 +19,7 @@ export default function HeaderCartButton({ onClick }) {
             </span>
             <span>Your Cart</span>
             <span className={styles.badge}>
-                3
+                {numberOfCartItems}
             </span>
         </button>
     );
