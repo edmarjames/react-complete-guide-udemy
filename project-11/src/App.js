7414db39637674277fc2689eee3fce4b6068b241
edmar.james <edmarjamesbautista@gmail.com>
2023-09-02 21:38:32 +0800

    Fetching data from API

---
+++ project-11/src/App.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react';
+
+import MoviesList from './components/MoviesList';
+import './App.css';
+
+function App() {
+
+  const [movies, setMovies] = useState([]);
+
+  function fetchMovieHandler() {
+    fetch('https://swapi.dev/api/films/')
+      .then(res => res.json())
+      .then(data => {
+        const transformedMovies = data?.results?.map(movieData => {
+          return {
+            id: movieData.episode_id,
+            title: movieData.title,
+            openingText: movieData.opening_crawl,
+            releaseDate: movieData.release_date
+          }
+        })
+        setMovies(transformedMovies);
+      });
+  };
+  // const dummyMovies = [
+  //   {
+  //     id: 1,
+  //     title: 'Some Dummy Movie',
+  //     openingText: 'This is the opening text of the movie',
+  //     releaseDate: '2021-05-18',
+  //   },
+  //   {
+  //     id: 2,
+  //     title: 'Some Dummy Movie 2',
+  //     openingText: 'This is the second opening text of the movie',
+  //     releaseDate: '2021-05-19',
+  //   },
+  // ];
+
+  // useEffect(() => {
+  //   fetchMovieHandler();
+  // }, []);
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMovieHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={movies} />
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
